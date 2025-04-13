c3a3acb4128a5339b07e36991406b5d1898d0fbb
Prashant-Sharma-2056 <73585820+Prashant-Sharma-2056@users.noreply.github.com>
2025-04-13 09:53:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ data = {}
 def get_raw_data(state, city):
     # URL you want to scrape AQI.in dashboard
     url = f"https://www.aqi.in/weather/india/{state}/{city}"
-    time.sleep(20)
+    #time.sleep(20)
     response = requests.get(url)
 
     # Check if request was successful
